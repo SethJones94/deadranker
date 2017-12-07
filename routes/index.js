89a83b44060c592ec6bb8cc4a7b305040ad61aac
SethJones94 <sethtyjones@gmail.com>
2017-12-07 15:05:52 -0500

    that moment when you're too lazy to come up with an accurate commit message

--- routes/index.js
+++ routes/index.js
@@ -8,25 +8,29 @@ router.get('/', function(req, res, next) {
   res.render('index', {user: req.user});
 });
 
+/* GET register page */
 router.get('/register', function(req, res) {
   res.render('register', { });
 });
 
+/* POST register */
 router.post('/register', function(req, res) {
   Account.register(new Account({username: req.body.username}), req.body.password, function(err, account) {
     if (err) {
       return res.render('register', {account: account});
     }
+/* passport local auth */
     passport.authenticate('local')(req, res, function () {
       res.redirect('/');
     });
   });
 });
 
+/* GET login page */
 router.get('/login', function(req, res) {
   res.render('login', { user: req.user });
 });
-
+/* POST login */
 router.post('/login', passport.authenticate('local'), function(req, res) {
   res.redirect('/');
 })
